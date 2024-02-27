f87dee8eece8e116075fe83578d98becaf99aff5
Dheeraj Tallapragada <tsaidheeraj@gmail.com>
2024-02-26 20:11:51 -0800

    phase1

---
+++ app.js
@@ -0,0 +1,57 @@
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar')
+
+menuBtn.onclick = () =>{
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+
+  var swiper = new Swiper(".team-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+          
+        },
+        768: {
+          slidesPerView: 2,
+          
+        },
+        1024: {
+          slidesPerView: 3,
+          
+        },
+      },
+  });
+
+  const resumeInput = document.getElementById('resume');
+  const fileLabel = document.getElementById('fileLabel');
+  const resumeLabel = document.createElement('label');
+  resumeLabel.setAttribute('for', 'resume');
+  resumeLabel.id = 'resumeLabel';
+  resumeLabel.innerText = 'Choose File';
+  
+  resumeInput.addEventListener('change', function() {
+    const files = resumeInput.files;
+    if (files.length > 0) {
+      fileLabel.style.color = '#44a1e9';
+      resumeLabel.classList.add('file-selected');
+      fileLabel.innerText = files[0].name;
+      resumeLabel.style.color = 'green';
+      resumeLabel.innerText = 'File Uploaded';
+    } else {
+      fileLabel.style.color = 'red';
+      resumeLabel.classList.remove('file-selected');
+      resumeLabel.innerText = 'Choose File';
+    }
+  });
+  
+  fileLabel.parentNode.insertBefore(resumeLabel, resumeInput.nextSibling);
