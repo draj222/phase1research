16fdf942ff8b5a51ac9f6174591e1bfe2a07cc39
Dheeraj Tallapragada <tsaidheeraj@gmail.com>
2024-06-30 23:33:24 -0700

    Initial commit

--- app.js
+++ app.js
@@ -1,48 +1,12 @@
 let menuBtn = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .flex .navbar')
+let navbar = document.querySelector('.header .flex .navbar');
 
-menuBtn.onclick = () =>{
+menuBtn.onclick = () => {
     menuBtn.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
 
-
-  // var swiper = new Swiper(".team-slider", {
-  //   spaceBetween: 20,
-  //   grabCursor: true,
-  //   loop:true,
-  //   pagination: {
-  //     el: ".swiper-pagination",
-  //     clickable: true,
-  //   },
-
-  //   on: {
-  //     slideChange: function () {
-  //         const paginationBullets = document.querySelectorAll('.team .swiper-pagination-bullet');
-  //         paginationBullets.forEach((bullet, index) => {
-  //             bullet.classList.toggle('swiper-pagination-bullet-active', index === swiper.activeIndex);
-  //         });
-  //     }
-  // },
-
-  //   breakpoints: {
-  //       540: {
-  //         slidesPerView: 1,
-          
-  //       },
-  //       768: {
-  //         slidesPerView: 2,
-          
-  //       },
-  //       1024: {
-  //         slidesPerView: 3,
-          
-  //       },
-  //     },
-  // });
-
-
-  var swiper = new Swiper(".team-slider", {
+var swiper = new Swiper(".team-slider", {
     spaceBetween: 20,
     grabCursor: true,
     loop: true,
@@ -53,14 +17,6 @@ menuBtn.onclick = () =>{
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
-    on: {
-      slideChange: function () {
-        const paginationBullets = document.querySelectorAll('.team .swiper-pagination-bullet');
-        paginationBullets.forEach((bullet, index) => {
-          bullet.classList.toggle('swiper-pagination-bullet-active', index === swiper.realIndex);
-        });
-      }
     },
     breakpoints: {
       540: {
@@ -73,20 +29,24 @@ menuBtn.onclick = () =>{
         slidesPerView: 3,
       },
     },
-  });
+    on: {
+      slideChange: function () {
+        const paginationBullets = document.querySelectorAll('.swiper-pagination-bullet');
+        paginationBullets.forEach((bullet, index) => {
+          bullet.classList.toggle('swiper-pagination-bullet-active', index === swiper.realIndex);
+        });
+      }
+    },
+});
 
-  
-  
-  
+const resumeInput = document.getElementById('resume');
+const fileLabel = document.getElementById('fileLabel');
+const resumeLabel = document.createElement('label');
+resumeLabel.setAttribute('for', 'resume');
+resumeLabel.id = 'resumeLabel';
+resumeLabel.innerText = 'Choose File';
 
-  const resumeInput = document.getElementById('resume');
-  const fileLabel = document.getElementById('fileLabel');
-  const resumeLabel = document.createElement('label');
-  resumeLabel.setAttribute('for', 'resume');
-  resumeLabel.id = 'resumeLabel';
-  resumeLabel.innerText = 'Choose File';
-  
-  resumeInput.addEventListener('change', function() {
+resumeInput.addEventListener('change', function() {
     const files = resumeInput.files;
     if (files.length > 0) {
       fileLabel.style.color = '#44a1e9';
@@ -99,6 +59,6 @@ menuBtn.onclick = () =>{
       resumeLabel.classList.remove('file-selected');
       resumeLabel.innerText = 'Choose File';
     }
-  });
-  
-  fileLabel.parentNode.insertBefore(resumeLabel, resumeInput.nextSibling);
+});
+
+fileLabel.parentNode.insertBefore(resumeLabel, resumeInput.nextSibling);
